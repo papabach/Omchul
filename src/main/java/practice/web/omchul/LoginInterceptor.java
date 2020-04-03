59a976fae6c59d828c8079a04427b0e5db0b40c4
papabach <59527978+papabach@users.noreply.github.com>
2020-04-03 17:33:14 +0900

    priactice1

---
+++ src/main/java/practice/web/omchul/LoginInterceptor.java
@@ -0,0 +1,44 @@
+package practice.web.omchul;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("logInUser")!=null) {
+			return true;
+		} else {
+			request.setAttribute("fail", "needLogin");
+			request.getRequestDispatcher("/logInf").forward(request, response);
+			return false;
+		}
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+	@Override
+	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		super.afterConcurrentHandlingStarted(request, response, handler);
+	}
+	
+	
+}
